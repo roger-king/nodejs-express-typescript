7ac3480c122d02dc29e76661b72e157f689e4539
Roger King <rogr.king@gmail.com>
2018-04-13 20:03:25 -0400

    tslint ignore on console log

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ const port = config.PORT;
 app.listen(port);
 app.on('error', onError);
 app.on('listening', () => {
+    // tslint:disable-next-line
     console.log(`Application is now running on http://localhost:${port}`);
 })
 
@@ -22,10 +23,12 @@ function onError(error: NodeJS.ErrnoException) {
     // handle specific listen errors with friendly messages
     switch (error.code) {
         case "EACCES":
+            // tslint:disable-next-line
             console.error(bind + " requires elevated privileges");
             process.exit(1);
             break;
         case "EADDRINUSE":
+            // tslint:disable-next-line
             console.error(bind + " is already in use");
             process.exit(1);
             break;
@@ -36,12 +39,15 @@ function onError(error: NodeJS.ErrnoException) {
 
 process.on("unhandledRejection", (e) => {
     if (e.fatal) {
+        // tslint:disable-next-line
         console.error(e);
         process.exit(1);
     }
     // Todo: Look back into how/why this is being unhandled.
     if (e.statusCode !== 304) {
+        // tslint:disable-next-line
         console.log("!!UNHANDLED:");
+        // tslint:disable-next-line
         console.error(e);
     }
 });
