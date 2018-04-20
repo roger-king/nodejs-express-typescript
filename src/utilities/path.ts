3f84287228c4637941047beb0c639a64ba9f40de
Roger King <rogr.king@gmail.com>
2018-04-19 20:05:08 -0400

    path util file for public folder

---
+++ src/utilities/path.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import { logger } from './logger';
+
+const publicPath = (): string => {
+    return path.join(__dirname, '../../public/');
+};
+
+export const webAppPath = () => {
+    return path.join(publicPath(), 'www');
+};
