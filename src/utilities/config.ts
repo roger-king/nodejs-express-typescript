a674f982deacb3a0562b271303907aeb7b2e5ccb
Roger King <rogr.king@gmail.com>
2018-04-23 07:31:13 -0400

    fix: config util

--- src/utilities/config.ts
+++ src/utilities/config.ts
@@ -1,35 +1,47 @@
 import { logger } from './logger';
 
-const requiredEnvVars: string[] = ['VERSION', 'PORT'];
+interface IConfig {
+    PORT: number;
+    VERSION: string;
+    LOG_LEVEL: string;
+}
 
-const isRequired = (variables: string[]): void => {
-    const missingEnvVars: string[] = [];
+const requiredEnvVars: string[] = ['PORT'];
+const missingEnvVars: string[] = [];
 
-    variables.forEach(variable => {
-        if (!process.env[variable]) {
-            missingEnvVars.push(variable);
+const isRequired = (configObj: any): void => {
+    for (const key in configObj) {
+        if (!configObj[key] && requiredEnvVars.indexOf(key) < 0) {
+            missingEnvVars.push(key);
         }
-    });
-
-    if (missingEnvVars.length) {
-        logger.error(`The following environment variables are required: ${missingEnvVars.join(', ')}`);
-        process.exit(1);
     }
 };
 
-const setupConfig = () => {
-    const appConfig = {
-        PORT: isLocal ? 4000 : process.env.PORT,
-        VERSION: isLocal ? 'local' : process.env.VERSION,
-        LOG_LEVEL: isLocal ? 'debug' : process.env.LOG_LEVEL,
+const getEnvVar = (localEnv, localEnvValue) => {
+    if (isLocal && localEnvValue) {
+        return localEnvValue;
+    }
+    return process.env[localEnv];
+};
+
+const setupConfig = (): IConfig => {
+    const version = require('./../../package.json').version;
+
+    const appConfig: IConfig = {
+        PORT: getEnvVar('PORT', 4000),
+        VERSION: isLocal ? 'local' : String(version),
+        LOG_LEVEL: getEnvVar('LOG_LEVEL', 'debug'),
     };
 
-    requiredEnvVars.forEach(variable => {
-        appConfig[variable] = !process.env[variable] ? appConfig[variable] : process.env[variable];
-    });
+    isRequired(appConfig);
 
     return appConfig;
 };
 
 export const isLocal: boolean = process.env.NODE_ENV !== 'production';
-export const config = setupConfig();
+export const config: IConfig = setupConfig();
+
+if (missingEnvVars.length) {
+    logger.error(`The following environment variables are required: ${missingEnvVars.join(', ')}`);
+    process.exit(1);
+}
